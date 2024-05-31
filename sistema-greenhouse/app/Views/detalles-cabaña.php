9530d4da85d8438590db94105662573b7ccafbe8
joaquin-ac <joaquin.acb36@gmail.com>
2024-05-31 15:48:02 -0300

    login, registro, footer

--- sistema-greenhouse/app/Views/detalles-cabaña.php
+++ sistema-greenhouse/app/Views/detalles-cabaña.php
@@ -1,34 +1,38 @@
 <style>
     p {
-    font-size: 20px;
-    text-align: justify;
+        font-size: 20px;
+        text-align: justify;
     }
 </style>
 
 <div class="container mt-5" style="color: white ;">
     <div class="row">
         <div class="col-md-6">
-        <div class="card">
-            <div class="card-body">
-            <h2 class="text-secondary"><?= $cabaña['nombre'] ?></h2>
-                        <p>Capacidad: <?= $cabaña["capacidad"] ?></p>
-                        <p>🛖Cocina totalmente equipada</p>
-                        <p>🛖Baño con bañera</p>
-                        <p>🛖Smart tv</p>
-                        <p>🛖Wifi</p>
-                        <p>🛖Calefacción a gas natural</p>
-                        <p>🛖Mesas y sillas de jardín</p>
-                        <p>🛖Estacionamiento al lado de la cabaña</p>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-secondary"><?= $cabaña['nombre'] ?></h2>
+                    <p>Capacidad: <?= $cabaña["capacidad"] ?></p>
+                    <p>🛖Cocina totalmente equipada</p>
+                    <p>🛖Baño con bañera</p>
+                    <p>🛖Smart tv</p>
+                    <p>🛖Wifi</p>
+                    <p>🛖Calefacción a gas natural</p>
+                    <p>🛖Mesas y sillas de jardín</p>
+                    <p>🛖Estacionamiento al lado de la cabaña</p>
+                </div>
+                <?php if (session()->get('isLoggedIn')): ?>
+                    <a href="#" class="btn btn-primary" style="background-color: #27D99D;">Reserva ahora</a>
+                <?php else: ?>
+                    <a href="<?= base_url()?>login" class="btn btn-primary" style="background-color: #27D99D;">Reserva ahora</a>
+                <?php endif; ?>
+
             </div>
-            <a href="#" class="btn btn-primary" style="background-color: #27D99D;">Reserva ahora</a>
-</div>
         </div>
         <div class="col-md-6">
             <br>
             <img src="<?= base_url('assets/img/' . $cabaña['imagen']) ?>" class="img-fluid" alt="Imagen de cabaña">
         </div>
-        
+
     </div>
 </div>
 <br><br><br>
-
